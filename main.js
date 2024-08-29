bcccf28367f91a172ccee1fc74fdc6305442cd16
pula39 <programmed_life@naver.com>
2024-08-30 02:27:25 +0900

    ㅁㄴㅇㄹ

--- main.js
+++ main.js
@@ -36,18 +36,26 @@ function getRandomNoise() {
 }
 
 function updateClicks(addNum = "False") {
-    $.ajax({
-        url: "https://4epk4wx25ffur6udpbnhrlglue0rhrdv.lambda-url.ap-northeast-2.on.aws/",
-        type: "POST",
-        data: JSON.stringify({ 'licking': addNum }),
-        success: function (returnData) {
-            console.log(returnData);
-            document.getElementById("counted").innerHTML = parseInt(returnData, 10).toLocaleString('en-US');
+    fetch("https://4epk4wx25ffur6udpbnhrlglue0rhrdv.lambda-url.ap-northeast-2.on.aws/", {
+        method: "POST", // 요청 방식
+        headers: {
+            "Content-Type": "application/json" // 요청 헤더에 JSON 타입 설정
         },
-        error: function () {
-            console.log("updateClicks error!");
-        }
+        body: JSON.stringify({ 'licking': addNum }) // JSON 형식으로 변환된 데이터
     })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json(); // 응답을 JSON으로 변환
+    })
+    .then(data => {
+        console.log(data);
+        document.getElementById("counted").innerHTML = parseInt(data, 10).toLocaleString('en-US');
+    })
+    .catch(error => {
+        console.error("updateClicks error!", error);
+    });
 }
 
 var run = setInterval(updateClicks, 10000);
